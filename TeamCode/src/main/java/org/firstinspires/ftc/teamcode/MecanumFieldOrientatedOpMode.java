d43931d7e9f15c1ab95a30dd97738b6d84a5236a
TiaraAbraham <tiara.abraham15@gmail.com>
2026-09-21 15:22:13 -0400

    mecanumdrive

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumFieldOrientatedOpMode.java
@@ -0,0 +1,18 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+
+public class MecanumFieldOrientatedOpMode extends OpMode {
+    MecanumDrive drive = new MecanumDrive();
+    @Override
+    public void init() {
+
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
